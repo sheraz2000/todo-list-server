2cab41d9388df7ae3fd6f12b39965f091be947bd
sheraz 2000 <www.muhammad.sheraz2000@gmail.com>
2026-05-16 00:11:45 +0500

    sdafasD

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoute from "./controller/routes/userroutes.js";
+import userRoute from "./routes/userroutes.js";
 import cors from "cors";
 
 const app = express();

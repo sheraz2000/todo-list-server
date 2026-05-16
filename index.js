a2bca21feaa70700de45e15b9bac0dce8f4ac2c1
sheraz 2000 <www.muhammad.sheraz2000@gmail.com>
2026-05-17 01:06:29 +0500

    fix vercel serverless

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
 app.get("/api/health", (req, res) => {
@@ -18,15 +17,13 @@ app.get("/api/health", (req, res) => {
 
 app.use("/api/user", userRoute);
 
-const port = process.env.PORT || 8000;
 const mongoUrl = process.env.MONGO_URL;
 
 mongoose
   .connect(mongoUrl)
   .then(() => {
     console.log("DB connected successfully");
-    app.listen(port, () => {
-      console.log(`Server running on port ${port}`);
-    });
   })
   .catch((error) => console.log(error));
+
+export default app;
